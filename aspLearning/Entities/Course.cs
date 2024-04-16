d70f3a00d54f77c378baf669a060d4a8a69f95d0
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-04-16 21:11:37 +0330

    #s1-e16-RegularExpression

--- aspLearning/Entities/Course.cs
+++ aspLearning/Entities/Course.cs
@@ -5,17 +5,17 @@ using Microsoft.EntityFrameworkCore;
 namespace aspLearning.Entities;
 
 [Table("MyCourse", Schema = "catalog")]
-[Index(nameof(Name), nameof(RecordNum), IsUnique = true,Name = "Multiple_Index")]
+[Index(nameof(Name), nameof(RecordNum), IsUnique = true, Name = "Multiple_Index")]
 public class Course(int id, string name, string description)
 {
     [Key] public int Id { get; set; } = id; //1 , 2 , 3
 
     //[ForeignKey(nameof(Author))]
-    public int? AuthorId { get; set; }  //nullable
+    public int? AuthorId { get; set; } //nullable
 
-    [ForeignKey(nameof(AuthorId))]
-    public Author Author { get; set; }
+    [ForeignKey(nameof(AuthorId))] public Author Author { get; set; }
 
+    [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "")] //@^$(*#&$()#*%_(
     public string Name { get; set; } = name;
 
     public int RecordNum { get; set; }
