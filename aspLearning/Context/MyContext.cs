fa6f5ebd5b7be5e3e0fa9c53e5ae6aaf02768a57
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-04-21 19:44:42 +0330

    #s2-e2- Table Names

--- aspLearning/Context/MyContext.cs
+++ aspLearning/Context/MyContext.cs
@@ -8,25 +8,12 @@ public class MyContext(DbContextOptions<MyContext> options) : DbContext(options)
     //entities
     public DbSet<Course> Courses => Set<Course>();
 
-    //Cache
-    //re-used
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         //Fluent API
         modelBuilder.Entity<User>()
-            .Property(x => x.UserName)
-            .HasMaxLength(150);
+            .ToTable("tbl_User", "catalog");
 
         base.OnModelCreating(modelBuilder);
-    }
-
-    //new context => call
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        // optionsBuilder.UseSqlServer(opt =>
-        // {
-        //
-        // });
-        base.OnConfiguring(optionsBuilder);
     }
 }
