3f40f1f80520271e224de075197606cf7fc45988
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-08-25 17:40:04 +0330

    #s12 - e3 model Class Parameters

--- MyController/Controllers/HomeController.cs
+++ MyController/Controllers/HomeController.cs
@@ -1,19 +1,18 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using MyController.Entities;
+using MyController.Models;
 
 namespace MyController.Controllers;
 
 public class HomeController : Controller
 {
     [Route("/")]
-    [Route("Index/{pageId:int?}/{searchBy}")]
-    //Query String : http://localhost:5183/Index?pageId=10&searchBy=AliChavoshi
-    //Route Data : http://localhost:5183/index/10/alichavoshi
-    //Mix : http://localhost:5183/index/10?searchBy=alichavoshi
-    public IActionResult Index([FromRoute] int? pageId, [FromQuery] string? searchBy)
+    [Route("Index")]
+    //From Query
+    public IActionResult Index(IndexParameters parameters)
     {
-        return Content($"pageId : {pageId} - searchBy : {searchBy}");
+        return Content($"pageId : {parameters.PageId} - searchBy : {parameters.SearchBy}");
     }
 
     [Route("Test")]
---
+++ MyController/Models/IndexParameters.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyController.Models;
+
+public class IndexParameters
+{
+    [FromQuery] public int? PageId { get; set; } = 10;
+    [FromQuery] public string? SearchBy { get; set; }
+}
