2c674994cc4190f375125886f2b1c6d1d3e33aa4
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-09-13 20:42:51 +0330

    #s12 - e5 - custom validators

---
+++ aspLearning/Attributes/ValidateDomainEmailAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspLearning.Attributes;
+
+//alichavoshi@gmail.com
+public class ValidateDomainEmailAttribute(string allowDomain, string errorMessage = "...") : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        var split = value?.ToString()!.Split("@"); // 0=> alichavoshi | 1 => gmail.com
+        return string.Equals(split![1], allowDomain, StringComparison.CurrentCultureIgnoreCase); //true | false
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var split = value?.ToString()!.Split("@"); // 0=> alichavoshi | 1 => gmail.com
+        var result = string.Equals(split![1], allowDomain, StringComparison.CurrentCultureIgnoreCase);
+        if (result) return ValidationResult.Success;
+        return new ValidationResult(string.Format(errorMessage ?? "please enter value"));
+    }
+}
--- aspLearning/Entities/Author.cs
+++ aspLearning/Entities/Author.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using aspLearning.Attributes;
 
 namespace aspLearning.Entities;
 
@@ -8,6 +9,9 @@ public class Author
 {
     [Key] public int Id { get; set; }
     public string Name { get; set; }
+
+    [ValidateDomainEmail("gmail.com","please write gmail.com domain")]
+    public string Email { get; set; }
 
     #region Relations
 
