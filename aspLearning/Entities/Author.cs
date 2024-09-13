28ed1b58489d97a07cbfc22c21ad77c2e9053611
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-09-13 20:59:01 +0330

    #s12 e6 IValidatable Object

--- aspLearning/Entities/Author.cs
+++ aspLearning/Entities/Author.cs
@@ -2,15 +2,17 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using aspLearning.Attributes;
+using Newtonsoft.Json.Linq;
 
 namespace aspLearning.Entities;
 
-public class Author
+public class Author : IValidatableObject
 {
     [Key] public int Id { get; set; }
-    public string Name { get; set; }
 
-    [ValidateDomainEmail("gmail.com","please write gmail.com domain")]
+    public string Name { get; set; } 
+
+    [ValidateDomainEmail("gmail.com", "please write gmail.com domain")]
     public string Email { get; set; }
 
     #region Relations
@@ -18,4 +20,11 @@ public class Author
     // public virtual List<Course> Courses { get; set; } //select many
 
     #endregion
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var split = Email.Split("@"); // 0=> alichavoshi | 1 => gmail.com
+        var result = string.Equals(split![1], "GMAIL.COM", StringComparison.CurrentCultureIgnoreCase);
+        if (result) yield return new ValidationResult("please enter valid domain");
+    }
 }
