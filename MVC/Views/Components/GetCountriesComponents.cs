1be2140880fcaf499606cf18302d768ac00da852
Ali Chavoshi <AliChavoshii1372@gmail.com>
2024-10-18 19:32:02 +0330

    #s15 - e1 view components

---
+++ MVC/Views/Components/GetCountriesComponents.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Views.Components;
+
+public class GetCountriesComponents : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync()
+    {
+        //Logic
+        return Task.FromResult<IViewComponentResult>(
+            View("/Views/Components/CountriesComponent.cshtml", "asp .net core from ALI")
+        );
+    }
+}
